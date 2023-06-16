696bf0db9d6be90049c0bcd6d068c60cdfc01305
Nignignina <131855794+Nignignina@users.noreply.github.com>
2023-06-16 13:59:12 +0200

    Add files via upload

--- 15_06_2023/src/App.jsx
+++ 15_06_2023/src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
 import TodoList from "./components/todoList";
-import { array } from "./array/array.js";
 
 function App() {
   return (
     <div className="App ">
-      eccola
       <TodoList filterAmount={16} />
       <TodoList filterAmount={36} />
       <TodoList filterAmount={56} />
--- 15_06_2023/src/components/todoList/TodoList.jsx
+++ 15_06_2023/src/components/todoList/TodoList.jsx
@@ -5,14 +5,33 @@ import { array } from "../../array/array.js";
 import TodoItem from "../todoItem/TodoItem";
 
 const TodoList = ({ filterAmount }) => {
-  const [todoList, setTodoList] = useState(
-    array.filter((todo) => todo.title.length <= filterAmount)
-  );
+  const [todoList, setTodoList] = useState(array.filter((todo) => todo));
+
+  const changeValue = () => {
+    setTodoList(array.filter((todo) => todo.title.length <= filterAmount));
+  };
+
+  const resetValue = () => {
+    setTodoList(array.filter((todo) => todo));
+  };
   return (
     <div className="TodoList">
       <div className="dataTodo">
-        <h2> Filtrata per {filterAmount} </h2>
-        <p>{todoList.length}</p>
+        <div className="buttonWrapp">
+          <p className="filter">Filter</p>
+          <button className="Button" onClick={changeValue}>
+            {" "}
+            Characters n° {filterAmount}
+          </button>
+          <button className="Button" onClick={resetValue}>
+            {" "}
+            Showall
+          </button>
+        </div>
+        <div className="total">
+          <p> Total:</p>
+          <p className="num"> {todoList.length}</p>
+        </div>
       </div>
       <hr />
 
